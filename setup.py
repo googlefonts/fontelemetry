09259e39d2e7336189a137f7ce39b6db1c798e3b
Chris Simpkins <git.simpkins@gmail.com>
2019-02-01 17:50:42 -0500

    [setup.py] refactor metadata definitions approach

--- setup.py
+++ setup.py
@@ -1,16 +1,67 @@
+import io
+import os
 import sys
 from setuptools import setup, find_packages
-from distutils.util import convert_path
+
+# Package meta-data.
+NAME = "fbReporter"
+DESCRIPTION = "A library and toolset for typeface software development reporting."
+LICENSE = "Apache License v2.0"
+URL = ""  # TODO
+EMAIL = "dcrossland@google.com"
+AUTHOR = "Font Bakery Reporter Authors and Contributors"
+REQUIRES_PYTHON = ">=3.6.0"
+
+# Optional packages
+EXTRAS = {
+}
+
+this_file_path = os.path.abspath(os.path.dirname(__file__))
 
 # Version
 main_namespace = {}
-version_path = convert_path("Lib/fbReporter/__init__.py")
-with open(version_path) as v:
-    exec(v.read(), main_namespace)
+version_fp = os.path.join(this_file_path, "Lib", "fbReporter", "__init__.py")
+try:
+    with io.open(version_fp) as v:
+        exec(v.read(), main_namespace)
+except IOError as version_e:
+    sys.stderr.write(
+        "[ERROR] setup.py: Failed to read the version data for the version definition: {}".format(
+            str(version_e)
+        )
+    )
+    raise version_e
 
-# Classifiers for PyPI
-classifiers = {
-    "classifiers": [
+# Use repository Markdown README.md for PyPI long description
+try:
+    with io.open("README.md", encoding="utf-8") as f:
+        readme = f.read()
+except IOError as readme_e:
+    sys.stderr.write(
+        "[ERROR] setup.py: Failed to read the README.md file for the long description definition: {}".format(
+            str(readme_e)
+        )
+    )
+    raise readme_e
+
+setup(
+    name=NAME,
+    version=main_namespace["__version__"],
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    license=LICENSE,
+    platforms=["Any"],
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    package_dir={"": "Lib"},
+    packages=find_packages("Lib"),
+    include_package_data=True,
+    extras_require=EXTRAS,
+    python_requires=REQUIRES_PYTHON,
+    entry_points={"console_scripts": ["fb-reporter = fbReporter.__main__:main"]},
+    classifiers=[
         "Development Status :: 3 - Alpha",
         "Framework :: Jupyter",
         "Environment :: Console",
@@ -25,41 +76,5 @@ classifiers = {
         "Topic :: Text Processing :: Fonts",
         "Topic :: Multimedia :: Graphics",
         "Topic :: Multimedia :: Graphics :: Graphics Conversion",
-    ]
-}
-
-# Extra, optional dependency packages
-extras_require = {}
-
-# Use repository Markdown README.md for PyPI long description
-try:
-    with open("README.md", encoding="utf-8") as f:
-        readme = f.read()
-except IOError as e:
-    sys.stderr.write(
-        "[ERROR] Failed to read the README.md file from git repository for long description definition: {}".format(
-            str(e)
-        )
-    )
-
-setup(
-    name="fb-reporter",
-    version=main_namespace["__version__"],
-    description="A library and toolset for typeface software development reporting",
-    author="Font Bakery Reporter Authors and Contributors",
-    author_email="dcrossland@google.com",
-    # TODO
-    url="***",
-    # TODO
-    license="***",
-    platforms=["Any"],
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    package_dir={"": "Lib"},
-    packages=find_packages("Lib"),
-    include_package_data=True,
-    extras_require=extras_require,
-    python_requires=">=3.6",
-    entry_points={"console_scripts": ["fb-reporter = fbReporter.__main__:main"]},
-    **classifiers
+    ],
 )
