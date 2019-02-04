a7f8ed7e508346c9433ab8244f86f549360e985b
Chris Simpkins <git.simpkins@gmail.com>
2019-02-04 08:58:12 -0500

    updated error message

--- Lib/fbReporter/__main__.py
+++ Lib/fbReporter/__main__.py
@@ -2,11 +2,14 @@ import os
 import runpy
 import sys
 
+import fontTools
+import glyphsLib
+
 
 def main():
     argv = sys.argv
     if len(argv) == 1:
-        sys.stderr.write("[ERROR] Missing arguments!{}".format(os.linesep))
+        sys.stderr.write("[ERROR] Missing arguments to fb-reporter!{}".format(os.linesep))
         sys.exit(1)
     # TODO : add support for --help, --usage, --version options
     module = "fbReporter." + argv[0]
