2a8185d65d3043024396bd9971f251739a06cf63
Chris Simpkins <git.simpkins@gmail.com>
2019-02-04 09:13:47 -0500

    [fb-reporter executable] add executable version command line flag support

--- Lib/fbReporter/__main__.py
+++ Lib/fbReporter/__main__.py
@@ -1,6 +1,9 @@
+import argparse
 import os
 import runpy
 import sys
+
+from fbReporter import __version__
 
 import fontTools
 import glyphsLib
@@ -11,7 +14,20 @@ def main():
     if len(argv) == 1:
         sys.stderr.write("[ERROR] Missing arguments to fb-reporter!{}".format(os.linesep))
         sys.exit(1)
-    # TODO : add support for --help, --usage, --version options
+    # TODO : add support for --help, --version options
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--version",
+                        help="display version number",
+                        action="store_true")
+
+    args = parser.parse_args()
+
+    # VERSION
+    if args.version:
+        print("fb-reporter v{}".format(__version__))
+        sys.exit(0)
+
+    # TODO: implement execution of module-specific code as command suite
     module = "fbReporter." + argv[0]
 
     runpy.run_module(module, run_name="__main__")
