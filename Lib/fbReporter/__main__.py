267cb24d3fd8d4d52049d874cbdfd8465989ec36
Chris Simpkins <git.simpkins@gmail.com>
2019-02-04 11:33:54 -0500

    add license to top level modules

--- Lib/fbReporter/__main__.py
+++ Lib/fbReporter/__main__.py
@@ -1,3 +1,17 @@
+# Copyright 2019 Font Bakery Reporter Authors and Contributors
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import argparse
 import os
 import runpy
@@ -16,6 +30,7 @@ def main():
             "[ERROR] Missing arguments to fb-reporter!{}".format(os.linesep)
         )
         sys.exit(1)
+    # argparse parser
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-v", "--version", help="display version number", action="store_true"
@@ -23,7 +38,7 @@ def main():
 
     args = parser.parse_args()
 
-    # VERSION
+    # command line logic implementation
     if args.version:
         print("fb-reporter v{}".format(__version__))
         sys.exit(0)
