787cfd2c798f3cbd15c121f627597c58880ac30b
Chris Simpkins <git.simpkins@gmail.com>
2019-02-04 11:36:14 -0500

    [datastructures.color] new GlyphsAppColor enum

---
+++ Lib/fbReporter/datastructures/color.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+
+class GlyphsAppColor(Enum):
+    RED = 0
+    ORANGE = 1
+    BROWN = 2
+    YELLOW = 3
+    LTGREEN = 4
+    DKGREEN = 5
+    LTBLUE = 6
+    DKBLUE = 7
+    PURPLE = 8
+    PINK = 9
+    LTGREY = 10
+    DKGREY = 11
+    WHITE = 12  # this is an uncolored glyph
+
+
+
