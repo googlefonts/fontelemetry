3a6c9622e90e1803496d63d7932753e08eb3d192
Chris Simpkins <git.simpkins@gmail.com>
2019-02-06 14:48:20 -0500

    [parsers.toml] refactored parse_toml to parse_toml_file, added parse_toml_string function

--- Lib/fbReporter/parsers/toml.py
+++ Lib/fbReporter/parsers/toml.py
@@ -4,6 +4,9 @@ from fbReporter import __version__
 import toml
 
 
-def parse_toml(file_object):
+def parse_toml_file(file_object):
     return toml.load(file_object)
 
+
+def parse_toml_string(toml_string):
+    return toml.loads(toml_string)
