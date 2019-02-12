b1d803e76e9305b21654c134db2a028afc9b784e
Chris Simpkins <git.simpkins@gmail.com>
2019-02-12 15:19:18 -0500

    [setup.py] fix install_requires fields bug
    missing comma in the list

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ INSTALL_REQUIRES = ["fontTools==3.35.0",
                     "fs<3,>=2.1.1",
                     "glyphsLib==3.1.4",
                     "iPython==7.2.0",
-                    "pandas==0.23.4"
+                    "pandas==0.23.4",
                     "plotly==3.6.0",
                     "notebook==5.7.0",
                     "toml==0.10.0"]
