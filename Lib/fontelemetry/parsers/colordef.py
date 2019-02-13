b6ba5bdc3cfdc8d69be23618974d64530e336fbe
Chris Simpkins <git.simpkins@gmail.com>
2019-02-13 16:28:12 -0500

    [parsers.colordef] add docstrings

--- Lib/fontelemetry/parsers/colordef.py
+++ Lib/fontelemetry/parsers/colordef.py
@@ -22,38 +22,62 @@ from fontelemetry.datastructures.color import GlyphsAppColor, GlyphsAppColorDefM
 
 # Base classes
 class ColorDefParser(object):
-    """"""
+    """Base class for glyph mark color definition parsers.
+
+    Attributes:
+        settings: settings dictionary that follows fontelemetry settings specification
+        colordef_map: fontelemetry.datastructures.color.GlyphsAppColorDefMap
+        colordef_settings: settings["colordefinitions"][SOURCE FORMAT] field of settings
+    """
+
     def __init__(self, settings):
+        """Inits ColorDefParser."""
         self.settings = settings
-        self.colordef = None
+        self.colordef_map = None
+        self.colordef_settings = None
 
     def __repr__(self):
-        return "({} v{} is defined as: {})".format(self.__class__, __version__, self.__dict__)
+        return "({} v{} is defined as: {})".format(
+            self.__class__, __version__, self.__dict__
+        )
 
     def __str__(self):
         return "{}".format(self.__dict__)
 
-    def _define_parser_attr(self):
+    def _define_parser_colordef_attr(self):
         raise NotImplementedError
 
     def parse(self):
+        """Parses data for glyph mark color definitions and returns a
+        fontelemetry.datastructures.color.GlyphsAppColorDefMap."""
         raise NotImplementedError
 
 
 # Inherited classes
 class ColorDefParserGlyphs(ColorDefParser):
+    """Parses glyph mark color definitions from glyphs source files.
+
+    Attributes:
+        settings: settings dictionary that follows fontelemetry settings specification
+        colordef_map: fontelemetry.datastructures.color.GlyphsAppColorDefMap
+        colordef_settings: settings["colordefinitions"][SOURCE FORMAT] field of settings
+    """
+
     def __init__(self, settings):
+        """Inits ColorDefParserGlyphs."""
         ColorDefParser.__init__(self, settings)
         self.valid_colors = [
             member.name for name, member in GlyphsAppColor.__members__.items()
         ]
-        self._define_parser_attr()
+        self._define_parser_colordef_attr()
 
-    def _define_parser_attr(self):
+    def _define_parser_colordef_attr(self):
         self.colordef_map = GlyphsAppColorDefMap()
         self.colordef_settings = self.settings["colordefinitions"]["glyphs"]
 
     def parse(self):
+        """Parses data for glyph mark color definitions in glyphs source files
+        and returns a fontelemetry.datastructures.color.GlyphsAppColorDefMap."""
         for color in self.colordef_settings:
             if color in self.valid_colors:
                 # retrieve the color definition from the settings data
