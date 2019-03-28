a0f4237a91c7d310026e66fc33fc6df3a39f26e3
Chris Simpkins <git.simpkins@gmail.com>
2019-03-28 15:27:49 -0400

    [converters.color] add rgb to hexadecimal string converter

--- Lib/fontelemetry/converters/color.py
+++ Lib/fontelemetry/converters/color.py
@@ -18,9 +18,15 @@ from fontelemetry import __version__
 
 import math
 
+# -----------------------------------------------
+#
+# Color conversions
+#
+# ------------------------------------------------
+
 
 def get_upscaled_rgb_tuple(downscaled_rgb_tuple):
-    """Scales an RGB color object from decimal 0.0-1.0 to int 0-255.
+    """Scales RGB color values from decimal 0.0-1.0 to int 0-255.
 
     Based on source by Greg Taylor in the python-colormath library
     https://github.com/gtaylor/python-colormath/blob/1d168613718d2d7d31ec4230524e987ef66823c7/colormath/color_objects.py#L565
@@ -33,10 +39,21 @@ def get_upscaled_rgb_tuple(downscaled_rgb_tuple):
 
 
 def get_downscaled_rgb_tuple(upscaled_rgb_tuple):
-    """Scales an RGB color object from int 0-255 to decimal 0.0-1.0."""
+    """Scales RGB color values from int 0-255 to decimal 0.0-1.0."""
     rgb_r_up, rgb_g_up, rgb_b_up = upscaled_rgb_tuple
     rgb_r = round(rgb_r_up / 255.0, 2)
     rgb_g = round(rgb_g_up / 255.0, 2)
     rgb_b = round(rgb_b_up / 255.0, 2)
     return rgb_r, rgb_g, rgb_b
 
+
+def get_hexadecimal_rgb_string(rgb_tuple):
+    """Converts RGB color values to a hexadecimal string"""
+    if type(rgb_tuple[0]) is float or type(rgb_tuple[1]) is float or type(rgb_tuple[2]) is float:
+        # convert to upscaled rgb values for the hexadecimal conversion
+        upscaled_rgb = get_upscaled_rgb_tuple(rgb_tuple)
+        rgb_r, rgb_g, rgb_b = upscaled_rgb
+    else:
+        rgb_r, rgb_g, rgb_b = rgb_tuple
+
+    return "{:02x}{:02x}{:02x}".format(rgb_r, rgb_g, rgb_b)
